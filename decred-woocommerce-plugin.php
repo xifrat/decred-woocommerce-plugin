33b1df1e527b3e2eeede0d43e471eae4d326ce5c
xifrat <>
2018-03-06 04:12:18 +0100

    Document & format with WordPress coding standards plugin init files

--- decred-woocommerce-plugin.php
+++ decred-woocommerce-plugin.php
@@ -1,22 +1,25 @@
 <?php
-/*
- Plugin Name: Decred direct payments for WooCommerce
- Plugin URI: https://github.com/xifrat/decred-woocommerce-plugin
- Description: Accept the Decred cryptocurrency on your WooCommerce store. Not a gateway, customers will send DCR directly to your wallet.
- Version: 0.1
- Author: xifrat
- Author URI:  https://github.com/xifrat
- Text Domain: decred
- License: ISC
- License URI: https://github.com/xifrat/decred-woocommerce-plugin/blob/master/LICENSE
+/**
+ *
+ * Plugin Name: Decred direct payments for WooCommerce
+ * Plugin URI: https://github.com/xifrat/decred-woocommerce-plugin
+ * Description: Accept the Decred cryptocurrency on your WooCommerce store. Not a gateway, customers will send DCR directly to your wallet.
+ * Version: 0.1
+ * Author: xifrat
+ * Author URI:  https://github.com/xifrat
+ * Text Domain: decred
+ * License: ISC
+ * License URI: https://github.com/xifrat/decred-woocommerce-plugin/blob/master/LICENSE
+ *
+ * @package Decred Payments
  */
 
-defined('ABSPATH') || exit; // prevent direct URL execution
+defined( 'ABSPATH' ) || exit; // prevent direct URL execution.
 
 include_once 'includes/class-decred-wc-plugin.php';
 
-$decred_plugin_name = plugin_basename(__FILE__);
+$decred_plugin_name = plugin_basename( __FILE__ );
 
-$decred_wc_plugin = new DecredWcPlugin($decred_plugin_name);
+$decred_wc_plugin = new Decred_WC_Plugin( $decred_plugin_name );
 
 $decred_wc_plugin->init();
--- includes/class-decred-wc-plugin.php
+++ includes/class-decred-wc-plugin.php
@@ -1,75 +1,112 @@
 <?php
+/**
+ * Decred Payments WooCommerce Plugin
+ *
+ * @package Decred Payments
+ */
 
-defined('ABSPATH') || exit;  // prevent direct URL execution
+defined( 'ABSPATH' ) || exit;  // prevent direct URL execution.
 
-class DecredWcPlugin {
-    
-    private $name;
-    private $operational;
-    private $logger;
-    
-    public function __construct($name) {
-        $this->name = $name;
-        // Plugin can be active but not operational if requirements are not met.
-        // Specifically if WooCommerce not active, we check that later.
-        $this->operational = false;
-    }
-    
-    public function init() {
-        add_action('plugins_loaded',                     [$this,'callback_plugins_loaded'], 0);
-        add_filter('woocommerce_payment_gateways',       [$this,'callback_add_payment_method']);
-        add_filter('plugin_action_links_' . $this->name, [$this,'callback_action_links']); 
-    }
-    
-    /**
-     * Initializations that depend on previous plugins being loaded.
-     * We specifically need the WooCommerce plugin classes (WC_*)
-     */
-    public function callback_plugins_loaded()
-    { 
-        // missing required WC classes, can't proceed
-        if (!class_exists('WC_Payment_Gateway') || !class_exists('WC_Logger')) {
-            return;
-        }
-        
-        include_once 'class-wc-decred-payments.php';
-        
-        $this->logger = new WC_Logger();
-        //$this->logger->debug(__METHOD__);
-        
-        $this->operational = true;
-    }
-    
-    /**
-     * Registers Decred payments class.
-     * 
-     * Settings form will show in WooCommerce Settings, Checkout tab.
-     */
-    public function callback_add_payment_method($methods)
-    {
-        if ($this->operational) {
-            $methods[] = 'WC_Decred_Payments';
-        }
-        return $methods;
-    }
-    
-    /**
-     * Adds settings & logs links to the plugin entry in the plugins menu
-     **/
-    public function callback_action_links($links)
-    {
-        if (!$this->operational) {
-            return $links;
-        }
-        
-        $log_file = 'decred-' . sanitize_file_name( wp_hash( 'decred' ) ) . '-log';
-        $logs_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=wc-status&tab=logs&log_file=' . $log_file . '">Logs</a>';
-        array_unshift($links, $logs_link);
-        
-        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=wc-settings&tab=checkout&section=wc_decred_payments">Settings</a>';
-        array_unshift($links, $settings_link);
-        
-        return $links;
-    }
-    
+/**
+ *
+ * Class created mostly to group together plugin configuration code.
+ *
+ * @author xifrat
+ */
+class Decred_WC_Plugin {
+
+	/**
+	 * Internal WordPress plugin name
+	 *
+	 * @var string name
+	 */
+	private $name;
+
+	/**
+	 * Is the plugin operational?
+	 *
+	 * Plugin can be active but not operational if requirements are not met.
+	 * Specifically if the WooCommerce plugin is not active (we check this after plugins loaded).
+	 *
+	 * @var boolean operational
+	 */
+	private $operational;
+
+	/**
+	 * Logger class, set after plugins loaded.
+	 *
+	 * @var WC_Logger operational
+	 */
+	private $logger;
+
+	/**
+	 * Constructor, de facto singleton, but we don't bother enforcing it.
+	 *
+	 * @param string $name Internal WordPress plugin name.
+	 */
+	public function __construct( $name ) {
+		$this->name        = $name;
+		$this->operational = false;
+	}
+
+	/**
+	 * Intialize plugin (set callbacks).
+	 */
+	public function init() {
+		add_action( 'plugins_loaded', [ $this, 'callback_plugins_loaded' ], 0 );
+		add_filter( 'woocommerce_payment_gateways', [ $this, 'callback_add_payment_method' ] );
+		add_filter( 'plugin_action_links_' . $this->name, [ $this, 'callback_action_links' ] );
+	}
+
+	/**
+	 * Initializations that depend on previous plugins being loaded.
+	 * We specifically need a number of WooCommerce plugin classes (WC_*).
+	 */
+	public function callback_plugins_loaded() {
+
+		if ( ! class_exists( 'WC_Payment_Gateway' ) || ! class_exists( 'WC_Logger' ) ) {
+			return; // missing required WC classes, can't proceed.
+		}
+
+		require_once 'class-wc-decred-payments.php';
+
+		$this->logger = new WC_Logger();
+
+		$this->operational = true;
+	}
+
+	/**
+	 * Registers Decred payments class.
+	 *
+	 * Settings form will show in WooCommerce Settings, Checkout tab.
+	 *
+	 * @param array $methods payment methods active in WooCommerce.
+	 */
+	public function callback_add_payment_method( $methods ) {
+		if ( $this->operational ) {
+			$methods[] = 'WC_Decred_Payments';
+		}
+		return $methods;
+	}
+
+	/**
+	 * Adds settings & logs links to the plugin entry in the plugins menu.
+	 *
+	 * @param array $links links already set by WordPress.
+	 **/
+	public function callback_action_links( $links ) {
+		if ( ! $this->operational ) {
+			return $links;
+		}
+
+		$log_file  = 'decred-' . sanitize_file_name( wp_hash( 'decred' ) ) . '-log';
+		$logs_link = '<a href="' . get_bloginfo( 'wpurl' ) . '/wp-admin/admin.php?page=wc-status&tab=logs&log_file=' . $log_file . '">Logs</a>';
+		array_unshift( $links, $logs_link );
+
+		$settings_link = '<a href="' . get_bloginfo( 'wpurl' ) . '/wp-admin/admin.php?page=wc-settings&tab=checkout&section=wc_decred_payments">Settings</a>';
+		array_unshift( $links, $settings_link );
+
+		return $links;
+	}
+
 }
