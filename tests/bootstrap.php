a31e0f6d2484934f52da33dd273600a7151fd0cd
xifrat <xifrat@jaumesola.com>
2018-04-14 10:46:52 +0200

    [devenv] Add some informative output to the PHPUnit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,8 @@
  * PHPUnit bootstrap file
  */
 
+echo "Bootstrap...\n";
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
@@ -15,7 +17,9 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 }
 
 // Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+$file = $_tests_dir . '/includes/functions.php';
+echo "Including $file ...\n";
+require_once $file;
 
 /**
  * Manually load the WooCommerce plugin, then the Decred Payments plugin.
@@ -27,4 +31,6 @@ function _manually_load_plugin() {
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+$file = $_tests_dir . '/includes/bootstrap.php';
+echo "Including $file ...\n";
+require_once $file;
