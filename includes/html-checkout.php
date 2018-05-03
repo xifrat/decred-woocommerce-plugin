3438c5a94308f449baa96063145495afbd087ddf
PavelZ <r3volut1oner@gmail.com>
2018-05-03 16:07:47 +0300

    Syntax fixes for checkout HTML

--- includes/html-checkout.php
+++ includes/html-checkout.php
@@ -13,43 +13,25 @@ defined( 'ABSPATH' ) || exit;  // prevent direct URL execution.
 		<span><?php echo __( 'Amount to pay:', 'decred' ); ?>&nbsp;</span>
 		<?php require __DIR__ . '/html-dcr-amount.php'; ?>
 	</span>
-<?php
-if ( $this->show_refund_address() ) {
-?>
-	<fieldset id="wc-decred-crypto-form" class="wc-payment-form">
-	<div class="form-row form-row-wide 
-				<?php
-				if ( $this->require_refund_address() ) {
-?>
-validate-required woocommerce-invalid<?php } ?>
-				checkout-decred-price">
-		<dl class="decred-refund-address-wrapper">
-			<dt>
-				<label for="decred-refund-address">
-				<br><?php echo __( 'Decred address for refunds', 'decred' ); ?>
-				<?php
-				if ( $this->require_refund_address() ) {
-?>
-<span class="required">*</span><?php } ?>
-				</label>
-			</dt>
-			<dd>
-				<input id="decred-refund-address" name="decred-refund-address"
-					class="input-text" style="width: 100%;" <?php /* data-bind="attr: {required: !isRefundAddressOptional()}" TODO implement JS required validation */ ?> 
-					autocomplete="off" autocorrect="no" autocapitalize="no" spellcheck="no"
-					required="true" type="text">	
-					<?php
-
-					/*
-					TODO implement JS required validation
-					<div id="decred-refund-address-error" class="decred-error-wrapper">
-					<span class="decred-error"><?= __('Please enter a valid Decred address','decred') ?></span>
-					</div>
-					*/
-?>
-			</dd>
-		</dl>
-	</div>
-	</fieldset>
-<?php
-}
+	<?php if ( $this->show_refund_address() ) : ?>
+        <fieldset id="wc-decred-crypto-form" class="wc-payment-form">
+        <div class="form-row form-row-wide <?php echo $this->require_refund_address() ? 'validate-required woocommerce-invalid' : ''; ?> checkout-decred-price">
+            <dl class="decred-refund-address-wrapper">
+                <dt>
+                    <label for="decred-refund-address">
+                    <br><?php echo __( 'Decred address for refunds', 'decred' ); ?>
+                        <?php if ( $this->require_refund_address() ) : ?>
+                            <span class="required">*</span>
+                        <?php endif; ?>
+                    </label>
+                </dt>
+                <dd>
+                    <input id="decred-refund-address" name="decred-refund-address"
+                        class="input-text" style="width: 100%;"
+                        autocomplete="off" autocorrect="no" autocapitalize="no" spellcheck="no"
+                        required="true" type="text">
+                </dd>
+            </dl>
+        </div>
+        </fieldset>
+    <?php endif; ?>
